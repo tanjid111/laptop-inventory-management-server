048e79df0d5141a970f473894ff038e22cb312c1
tanjid111 <tanjidhossain@gmail.com>
2022-05-07 14:38:11 -0700

    env created and connected

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.40i2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Laptop Inventory Server')
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
